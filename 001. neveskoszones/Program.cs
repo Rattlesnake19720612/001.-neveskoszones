0f982f0a4ff9b06803b0e2c5b0f3a82b50d95fc3
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 19:21:44 +0100

    Név bekérése és köszönés

---
+++ 001. neveskoszones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001.neveskoszones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem a neved és köszönök neked!");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Hali, csá, csövi " + nev + "!");
+
+            Console.ReadLine();
+        }
+    }
+}
